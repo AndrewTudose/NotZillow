5f1c57d1dffcb403d67d72e9ffe0456a82edc81f
AndrewToduse <andreitudose299@gmail.com>
2024-02-29 16:28:32 +0200

    #2 - Remove Edit and Delete from main Listing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -35,48 +35,8 @@ class ListingController extends Controller
         );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        // $this->authorize('create', Listing::class);
-
-        return inertia('Listing/Create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        // Listing::create( | OLD
-        $request->user()->listings()->create(
-            $request->validate([
-                'beds' => 'required|integer|min:1|max:20',
-                'baths' => 'required|integer|min:1|max:20',
-                'area' => 'required|integer|min:15|max:15000',
-                'city' => 'required',
-                'code' => 'required',
-                'street' => 'required',
-                'street_nr' => 'required|min:1|max:1000',
-                'price' => 'required|integer|min:1|max:20000000',
-            ])
-        );
-
-        return redirect()->route('listing.index')->with('success', 'Listing was created');
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Listing $listing)
     {
-        // if (Auth::user()->cannot('view', $listing)) {
-        //     abort(403);
-        // }
-
-        // $this->authorize('view', $listing);
 
         return inertia(
             'Listing/Show',
@@ -85,45 +45,4 @@ class ListingController extends Controller
             ]
         );
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Listing $listing)
-    {
-        return inertia(
-            'Listing/Edit',
-            [
-                'listing' => $listing
-            ]
-        );
-    }
-
-
-    public function update(Request $request, Listing $listing)
-    {
-        $listing->update(
-            $request->validate([
-                'beds' => 'required|integer|min:1|max:20',
-                'baths' => 'required|integer|min:1|max:20',
-                'area' => 'required|integer|min:15|max:15000',
-                'city' => 'required',
-                'code' => 'required',
-                'street' => 'required',
-                'street_nr' => 'required|min:1|max:1000',
-                'price' => 'required|integer|min:1|max:20000000',
-            ])
-        );
-
-        return redirect()->route('listing.index')
-            ->with('success', 'Listing was changed!');
-    }
-
-    // public function destroy(Listing $listing)
-    // {
-    //     $listing->delete();
-
-    //     return redirect()->back()
-    //         ->with('success', 'Listing was deleted!');
-    // }
 }
